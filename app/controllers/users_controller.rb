3e5bf12475f87050b42389d5d6dcaf91a102a3a4
鈴木俊亮 <g1530124@gmail.com>
2018-02-28 18:15:22 +0530

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @categories = Category.all
     @products = Product.all
   end
+
+  def show
+    # @forder = Finalorder.where[id: session[:myid]]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -76,6 +76,7 @@ root "users#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/users/home"
+  get "/users/show"
 
 
 end
